9e39aa1f53ff7d311daf7b8acd2d70973c5783d4
Markus Pyhäranta <markus.pyharanta@gmail.com>
2016-12-12 13:54:48 +0000

    Lisäys

---
+++ templates/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+        <title>Testisivu</title>
+        <meta charset="utf-8" />
+</head>
+<body>
+        <h1>Käyttäjän testisivu</h1>
+        <p>Tämä sama HTML-pohja näkyy kaikille käyttäjille.</p>
+
+        <?php
+        echo "Hello World in Php!";
+        ?>
+
+</body>
+</html>
